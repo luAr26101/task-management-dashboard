f8eea9c2db7ae4221364a56240b679ce1e20ebbf
luAr26101 <raul.cosmin.26@icloud.com>
2025-10-13 21:05:44 +0300

    Add folder structure and prettier plugin.

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import { Button } from "./components/ui/button";
 
 function App() {
   return (
-    <div className='flex min-h-svh flex-col items-center justify-center'>
-      <Button>Click me</Button>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button variant="default" size="lg">
+        Click me
+      </Button>
     </div>
   );
 }
